a761baa474376db7ca20a231f2690c77c8bf483f
xacffm <41066556+xacffm@users.noreply.github.com>
2023-09-06 08:46:25 +0200

    add summernote cleaner

--- admin/view/javascript/summernote/opencart.js
+++ admin/view/javascript/summernote/opencart.js
@@ -22,8 +22,8 @@ $(document).ready(function() {
 			toolbar: [
 				['style', ['style']],
 				['font', ['bold', 'underline', 'clear']],
-				['fontname', ['fontname']],
-				['fontsize', ['fontsize']],
+				// ['fontname', ['fontname']],
+				// ['fontsize', ['fontsize']],
 				['color', ['color']],
 				['para', ['ul', 'ol', 'paragraph']],
 				['table', ['table']],
@@ -37,7 +37,21 @@ $(document).ready(function() {
 					['float', ['floatLeft', 'floatRight', 'floatNone']],
 					['remove', ['removeMedia']]
 				],
-			},			
+			},
+			// cleaner:{
+			// 	  action: 'both', // both|button|paste 'button' only cleans via toolbar button, 'paste' only clean when pasting content, both does both options.
+			// 	  newline: '<br>', // Summernote's default is to use '<p><br></p>'
+			// 	  notStyle: 'position:absolute;top:0;left:0;right:0', // Position of Notification
+			// 	  icon: '<i class="note-icon">delete</i>',
+			// 	  keepHtml: false, // Remove all Html formats
+			// 	  keepOnlyTags: ['<p>', '<br>', '<ul>', '<li>', '<b>', '<strong>','<i>'], // If keepHtml is true, remove all tags except these
+			// 	  keepClasses: false, // Remove Classes
+			// 	  badTags: ['style', 'script', 'applet', 'embed', 'noframes', 'noscript', 'html'], // Remove full tags with contents
+			// 	  badAttributes: ['style', 'start'], // Remove attributes from remaining tags
+			// 	  limitChars: false, // 0/false|# 0/false disables option
+			// 	  limitDisplay: 'both', // text|html|both
+			// 	  limitStop: false // true/false
+			// },			
 			buttons: {
     			image: function() {
 					var ui = $.summernote.ui;
