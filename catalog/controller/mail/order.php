3a3af344d301cffd3d931271c430a05c669fbe34
vunamweb <vukynamkhtn@gmail.com>
2023-11-24 16:49:21 +0700

    pdf
    add first and last name

--- catalog/controller/mail/order.php
+++ catalog/controller/mail/order.php
@@ -160,9 +160,12 @@ class ControllerMailOrder extends Controller {
 			'zone_code' => $order_info['payment_zone_code'],
 			'country'   => $order_info['payment_country']
 		);
-
+  
 		$data['payment_address'] = str_replace(array("\r\n", "\r", "\n"), '<br />', preg_replace(array("/\s\s+/", "/\r\r+/", "/\n\n+/"), '<br />', trim(str_replace($find, $replace, $format))));
-
+		
+		$data['first_name'] = $order_info['payment_firstname'];
+		$data['last_name'] = $order_info['payment_lastname'];
+		
 		if ($order_info['shipping_address_format']) {
 			$format = $order_info['shipping_address_format'];
 		} else {
