156461e1a95c4d5a0dae0e25e502c0afd03bed03
vunamweb <vukynamkhtn@gmail.com>
2023-12-01 12:52:17 +0700

    fix comment
    - space between name, address
    - remove tax on checkout confirm

--- catalog/language/de-DE/checkout/success.php
+++ catalog/language/de-DE/checkout/success.php
@@ -14,5 +14,5 @@ $_['heading_title']	= 'Bestellung erfolgreich eingegangen';
 $_['text_basket']	= 'Warenkorb';
 $_['text_checkout']	= 'Kasse';
 $_['text_success']	= 'Erfolgreich';
-$_['text_customer']	= '<p>Bestellung wurde erfolgreich entgegengenommen und wird schnellstmöglich bearbeitet.<br />Der weitere Verlauf kann über <a href="%s">Mein Konto</a> &gt; <a href="%s">Verlauf</a> angesehen werden.<br />Befand sich im Warenkorb ein Downloadartikel kann dieser über <a href="%s">Downloads</a> abgerufen werden.</p><p>Für weitere Fragen bitte mit uns <a href="%s">Kontakt aufnehmen</a>.</p><p>Vielen Dank für den Einkauf bei uns.</p>';
+$_['text_customer']	= '<p>Bestellung wurde erfolgreich entgegengenommen und wird schnellstmöglich bearbeitet.<br />Der weitere Verlauf kann über <a href="%s">Mein Konto</a> &gt; <a href="%s">Verlauf</a> angesehen werden.<br /></p><p>Für weitere Fragen bitte mit uns <a href="%s">Kontakt aufnehmen</a>.</p><p>Vielen Dank für den Einkauf bei uns.</p>';
 $_['text_guest']	= '<p>Bestellung wurde erfolgreich entgegengenommen und wird schnellstmöglich bearbeitet.<br />Für weitere Fragen bitte mit uns <a href="%s">Kontakt aufnehmen</a>.</p><p>Vielen Dank für den Einkauf bei uns.</p>';
