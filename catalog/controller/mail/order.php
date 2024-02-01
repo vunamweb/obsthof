4b2ae3395d4a2526a6883d5e85b03c1fc805e749
vunamweb <vukynamkhtn@gmail.com>
2024-02-01 12:26:36 +0700

    invoice
    just set invoice after order if status is completed

--- catalog/controller/mail/order.php
+++ catalog/controller/mail/order.php
@@ -201,8 +201,9 @@ class ControllerMailOrder extends Controller {
 		$data['shipping_address'] = str_replace(array("\r\n", "\r", "\n"), '<br />', preg_replace(array("/\s\s+/", "/\r\r+/", "/\n\n+/"), '<br />', trim(str_replace($find, $replace, $format))));
 
 		$this->load->model('tool/upload');
-
-		// Products
+		$this->load->model('checkout/order');
+		
+        // Products
 		$data['products'] = array();
 
 		$sum_tax_1 = 0; $sum_tax_2 = 0; $totalNormalProduct = 0;
@@ -353,19 +354,37 @@ class ControllerMailOrder extends Controller {
 
 		$order_info['invoice_prefix'] = $invoice_prefix[0] . '-' . $invoice_prefix[1] . '-' . $invoice_prefix[2];
 
-		$invoiceNumber = $order_info['invoice_prefix'] . $order_info['invoice_no'];
+		//$invoiceNumber = $order_info['invoice_prefix'] . $order_info['invoice_no'];
 
-		$data['order_id'] = $invoiceNumber;
+		//$data['order_id'] = $invoiceNumber;
 		
-        $type = $this->createPDFInvoice($order_info, $order_status_id, $sum_tax_1, $sum_tax_2, $totalNormalProduct);
+        //$type = $this->createPDFInvoice($order_info, $order_status_id, $sum_tax_1, $sum_tax_2, $totalNormalProduct);
 		
-		$subject = html_entity_decode(sprintf($language->get('text_subject'), $order_info['store_name'], $invoiceNumber), ENT_QUOTES, 'UTF-8');
+		$subject = html_entity_decode(sprintf($language->get('text_subject'), $order_info['store_name'], $order_info['order_id']), ENT_QUOTES, 'UTF-8');
 		$fromName = html_entity_decode($order_info['store_name'], ENT_QUOTES, 'UTF-8');
 
-		$this->sendMailSMTP($order_info['email'], $subject, '', $fromName, $this->load->view('mail/order_add', $data), 1, $type);
+		// if status is complete
+		if(in_array($order_status_id, $this->config->get('config_complete_status'))) {
+		  $invoice = $this->model_checkout_order->countInvoiceNumber() + 1;
 
-		//if(true)
-		//if($order_status_id == 19)
+		  $this->model_checkout_order->setInvoiNumber($order_info['order_id'], $invoice);
+
+		  $invoiceNumber = $order_info['invoice_prefix'] . $invoice;
+
+		  $data['order_id'] = $invoiceNumber;
+
+		  $subject = html_entity_decode(sprintf($language->get('text_subject'), $order_info['store_name'], $invoiceNumber), ENT_QUOTES, 'UTF-8');
+
+		  $type = $this->createPDFInvoice($order_info, $order_status_id, $sum_tax_1, $sum_tax_2, $totalNormalProduct);
+		
+		  $this->sendMailSMTP($order_info['email'], $subject, '', $fromName, $this->load->view('mail/order_add', $data), 1, $type);
+		}
+		// if status is not complete
+		else {
+		   $type = $this->createPDFInvoice($order_info, $order_status_id, $sum_tax_1, $sum_tax_2, $totalNormalProduct);
+		   $this->sendMailSMTP($order_info['email'], $subject, '', $fromName, $this->load->view('mail/order_add', $data), 1);
+		} 
+
 		$this->sendMailSMTP(SPECIAL_EMAIL, $subject, '', $fromName, $this->load->view('mail/order_alert', $data), 1);
     }
 
@@ -411,6 +430,10 @@ class ControllerMailOrder extends Controller {
   }
 
   public function createPDFInvoice($order_info, $order_status_id, $sum_tax_1 = 0, $sum_tax_2 = 0, $totalNormalProduct = 0) {
+	//$this->load->model('checkout/order');
+
+	$order_info = $this->model_checkout_order->getOrder($order_info['order_id']);
+
 	//print_r($order_info); die();  
 	$invoice_no = $this->model_checkout_order->countInvoiceNumber() + 1;
 
@@ -803,12 +826,23 @@ class ControllerMailOrder extends Controller {
 		 
 		$checkStatus = (!in_array($order_status_id, $this->config->get('config_processing_status')) && !in_array($order_status_id, $this->config->get('config_complete_status'))) ? true : false;
 
-		if($order_status_id == 18 || $order_status_id == 5 || $checkStatus)
+		// if status is complete
+		if(in_array($order_status_id, $this->config->get('config_complete_status')))
 		  $this->sendMailSMTP($order_info['email'], $subject, '', $from, $template, 1, $type);
+		// if status is in process or cancel
+		else if(in_array($order_status_id, $this->config->get('config_processing_status')) || $checkStatus)
+		  $this->sendMailSMTP($order_info['email'], $subject, '', $from, $template, 1);
+		// status not in above
 		else 
 		  $this->sendMailSMTP($order_info['email'], $subject, '', $from, $template, 2, $type);
+		    
+		/*if($order_status_id == 18 || $order_status_id == 5 || $checkStatus)
+		  $this->sendMailSMTP($order_info['email'], $subject, '', $from, $template, 1, $type);
+		else 
+		  $this->sendMailSMTP($order_info['email'], $subject, '', $from, $template, 2, $type);*/
 		   	  
-		if($order_status_id == 18 || $order_status_id == 5 || $checkStatus)
+		// if status is complate or cancel
+		if(in_array($order_status_id, $this->config->get('config_complete_status')) || $checkStatus)
 		  $this->sendMailSMTP(SPECIAL_EMAIL, $subject, '', $from, $template, 1);
 	}
 	
